12168a869bf1c4a8bd53d64f95a5a492bbff024a
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-10 17:02:41 +0200

    Views of ref, reactive and computed prop using TS

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,25 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+import { reactive } from "vue";
+import User from "@/types/User";
+//Esempio di funzione in TS
+//const sum = (x: number, y: numberber) => x + y;
+
+//in caso di errore ci viene subito segnalato
+//sum(1, 3);
+
+const user: User = reactive({
+  id: 1,
+  username: "pippo",
+  settings: [],
+});
+
+console.log(user);
 </script>
 
 <template>
+  {{ user.id }}
   <main class="container m-auto p-10">
     <TheHeader />
     <EntryEditor />
--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,13 +1,28 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import { Ref, ref, computed } from "vue";
+import Emoji from "@/types/Emoji";
+
+const text = ref("");
+
+//Settiamo il valorer di emoji tipizzando con Emoji o null
+const emoji: Ref<Emoji | null> = ref(null);
+//Non possiamo quindi assegnare valori diversi da quelli specificati nel modello
+//Errore
+//emoji.value = "sdsp"
+
+const chartCount = computed(() => text.value.length);
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+    <textarea
+      v-model="text"
+      placeholder="New Journal Entry for danielkelly_io"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ chartCount }} / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
